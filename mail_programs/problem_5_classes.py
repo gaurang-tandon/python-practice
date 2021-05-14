5378f63727de72a020ec32efe43b31b3d509c85c
Gaurang Tandon <gaurang.tandon@gmail.com>
2021-05-14 13:51:28 +0530

    8/10 complete 2.0

--- mail_programs/problem_5_classes.py
+++ mail_programs/problem_5_classes.py
@@ -10,18 +10,26 @@ Also  include simple test function to test the class methods.
 
 
 class StringTest:
-    def __init__(self, age, gender):
+    def __init__(self, number, age, gender):
+        self.number = number
         self.age = age
         self.gender = gender
 
-        def get_string(self):
-            self.fname = input("Enter First Name : ")
+        def get_string():
+            self.fname = input(f"Enter First Name of {self.number} person : ")
             self.lname = input("Enter Last Name : ")
-        get_string(self)
+
+        get_string()
 
     def print_string(self):
-        print(self.fname.upper(), "is a", self.gender, "and is", self.age, "years old")
+        print(f"{self.fname} {self.lname} is a {self.gender} and is {self.age} years old".upper())
 
 
-gaurang = StringTest(25, "Male")
-gaurang.print_string()
+object_1 = StringTest(1, 25, "Male")
+object_2 = StringTest(2, 24, "Female")
+object_3 = StringTest(3, 26, "Male")
+
+object_1.print_string()
+object_2.print_string()
+object_3.print_string()
+
